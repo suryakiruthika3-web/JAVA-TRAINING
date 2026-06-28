95ac82ca6cd04d2570788e115bfab8be821c3d0a
suryakiruthika3-web <suryakiruthika3@gmail.com>
2026-06-28 19:28:00 +0530

    Conditional statements

---
+++ Day3/Problem01.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class Problem01 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter number: ");
+        int number = scanner.nextInt();
+        if (number < 0) {
+            System.out.println("Number cannot be negative.");
+        }
+    }
+}
