acff82917751a623f76ce8b6f1be68687d74802c
suryakiruthika3-web <suryakiruthika3@gmail.com>
2026-06-28 19:39:02 +0530

    Conditional Statements

--- Day1/Problem2.java
+++
@@ -1,5 +0,0 @@
-public class Problem2 {
-    public static void main(String[] args) {
-        System.out.print("26");
-    }
-}
--- Day1/Problem4.java
+++
@@ -1,6 +0,0 @@
-public class Problem4 {
-    public static void main(String[] args){
-        int a = 10;
-        System.out.println("Value of a: " + a);
-    }
-}
