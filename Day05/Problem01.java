3b9fccc52f7d2e6eef38b7da53914c4564351960
suryakiruthika3-web <suryakiruthika3@gmail.com>
2026-07-15 19:42:37 +0530

    Pattern Problems

---
+++ Day05/Problem01.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class Problem01 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int n = scanner.nextInt();
+        for(int i=1;i<=n;i++) {
+            for(int j=0;j<i;j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
