ba239f881a320715c381c7309c2b4bb3bf1091f0
suryakiruthika3-web <suryakiruthika3@gmail.com>
2026-07-15 19:49:15 +0530

    Remove folder_name

--- Day2/Problem13.java
+++
@@ -1,9 +0,0 @@
-import java.util.Scanner;
-public class Problem13 {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("Enter a number: ");
-        int number = scanner.nextInt();
-        System.out.println("The last number of the given number is: " + (number % 10));
-    }
-}
